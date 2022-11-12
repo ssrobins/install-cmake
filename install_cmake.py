f2070ce92e1fabee2e40d705f9ae83918a69bb1b
Steve Robinson <ssrobins@gmail.com>
2022-11-11 22:21:15 -0800

    Add pylint check

--- install_cmake.py
+++ install_cmake.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python3
 
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import argparse
 import os
 import platform
 import re
-import requests
 import subprocess
 import sys
 import tarfile
 import zipfile
+from urllib.request import build_opener, HTTPCookieProcessor, install_opener, Request, urlopen
 from bs4 import BeautifulSoup
 from packaging import version
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from urllib.request import build_opener, HTTPCookieProcessor, install_opener, Request, urlopen
+from urllib3.util import Retry
+import requests
 
 
 class CMakeInstall:
-    def __init__(self, cmake_version, rc):
-        if rc:
-            self.rc = True
+    def __init__(self, cmake_version, release_candidate):
+        if release_candidate:
+            self.release_candidate = True
         else:
-            self.rc = False
+            self.release_candidate = False
         if cmake_version:
             self.version = cmake_version
         else:
             self.version = self.get_latest_cmake_version()
 
+        self.cmake_url = "https://github.com/Kitware/CMake/releases"
+
         minimum_version = "3.20.0"
         if version.parse(self.version) < version.parse(minimum_version):
-            print(f"CMake {self.version} is not supported, the version must be {minimum_version} or higher.", flush=True)
-            print("If you'd like to make a case for broader support, please post to:", flush=True)
+            print(f"CMake {self.version} is not supported, "
+                "the version must be {minimum_version} or higher.", flush=True)
+            print("If you'd like to make a case for broader support, please post to:",
+            flush=True)
             print("https://github.com/ssrobins/install-cmake/issues", flush=True)
             sys.exit(1)
-
-        self.download_timeout_seconds = 10
-        self.download_retry_count = 3
-        self.download_backoff_factor = 10
 
         if platform.system() == "Darwin":
             cmake_platform = "macos-universal"
@@ -53,14 +57,14 @@ class CMakeInstall:
 
         cmake_dir = f"cmake-{self.version}-{cmake_platform}"
         self.archive = f"{cmake_dir}{cmake_archive_ext}"
-        self.url = f"https://github.com/Kitware/CMake/releases/download/v{self.version}/{self.archive}"
+        self.url = f"{self.cmake_url}/download/v{self.version}/{self.archive}"
         self.script_path = os.path.dirname(os.path.realpath(__file__))
         self.path = os.path.join(self.script_path, cmake_dir, cmake_binary_dir)
 
 
     def suitable_release_found(self, release_string):
         release_found = False
-        if ((self.rc and "Release Candidate" in release_string) or
+        if ((self.release_candidate and "Release Candidate" in release_string) or
             ("Latest Release" in release_string)):
             release_found = True
         return release_found
@@ -69,15 +73,17 @@ class CMakeInstall:
     def get_latest_cmake_version(self):
         opener = build_opener(HTTPCookieProcessor())
         install_opener(opener)
-        req = Request("https://cmake.org/download/", headers={"User-Agent": "Mozilla/72 (X11; Linux i686)"})
-        page = urlopen(req).read().decode('utf8', errors='ignore')
-        soup = BeautifulSoup(page, "html.parser")
+        req = Request("https://cmake.org/download/",
+            headers={"User-Agent": "Mozilla/72 (X11; Linux i686)"})
+        with urlopen(req) as response:
+            page = response.read().decode('utf8', errors='ignore')
+            soup = BeautifulSoup(page, "html.parser")
+            h3_tag_contents = soup.findAll("h3")
 
-        h3_tag_contents = soup.findAll("h3")
         version_text_raw = None
-        for h3_tag_contents in h3_tag_contents:
-            if self.suitable_release_found(h3_tag_contents.text):
-                version_text_raw = h3_tag_contents.text
+        for h3_tag_content in h3_tag_contents:
+            if self.suitable_release_found(h3_tag_content.text):
+                version_text_raw = h3_tag_content.text
                 break
 
         cmake_version = version_text_raw.split()[2].strip("()")
@@ -86,28 +92,33 @@ class CMakeInstall:
 
     def get_installed_cmake_version(self, cmake_version_output):
         cmake_version = ""
-        match = re.search("\d+\.\d+\.\d+(\-rc\d+)?", cmake_version_output)
+        match = re.search(r"\d+\.\d+\.\d+(\-rc\d+)?", cmake_version_output)
         if match:
             cmake_version = match.group(0)
         return cmake_version
 
 
     def requested_cmake_is_different(self):
-        cmake_version_output = subprocess.run("cmake --version", shell=True, check=True, stdout=subprocess.PIPE)
+        cmake_version_output = subprocess.run(
+            "cmake --version", shell=True, check=True, stdout=subprocess.PIPE)
         cmake_version_output = cmake_version_output.stdout.decode("utf-8")
         installed_cmake_version = self.get_installed_cmake_version(cmake_version_output)
         if installed_cmake_version == self.version:
             print(f"Requested CMake {self.version} matches what's already installed", flush=True)
             return False
-        else:
-            print(f"Requested CMake is {self.version} while {installed_cmake_version} is currently installed", flush=True)
-            return True
+        print(f"Requested CMake is {self.version} while {installed_cmake_version} "
+            "is currently installed", flush=True)
+        return True
 
 
     def download(self):
+        download_timeout_seconds = 10
+        download_retry_count = 3
+        download_backoff_factor = 10
+
         retry_strategy = Retry(
-            total=self.download_retry_count,
-            backoff_factor=self.download_backoff_factor,
+            total=download_retry_count,
+            backoff_factor=download_backoff_factor,
         )
         adapter = HTTPAdapter(max_retries = retry_strategy)
         http = requests.Session()
@@ -116,16 +127,16 @@ class CMakeInstall:
 
         try:
             print(f"Downloading {self.url}", flush=True)
-            request = http.get(self.url, timeout=self.download_timeout_seconds, stream=True)
+            request = http.get(self.url, timeout=download_timeout_seconds, stream=True)
             request.raise_for_status()
         except requests.exceptions.RequestException as error:
             print("Download failed", flush=True)
-            print("Check https://github.com/Kitware/CMake/releases to make sure you have a valid version")
+            print(f"Check {self.cmake_url} to make sure you have a valid version")
             print(error, flush=True)
-            exit(1)
+            sys.exit(1)
 
-        with open(self.archive, 'wb') as f:
-            f.write(request.content)
+        with open(self.archive, 'wb') as cmake_zip:
+            cmake_zip.write(request.content)
 
 
     def extract(self):
@@ -134,18 +145,18 @@ class CMakeInstall:
             with tarfile.open(f"{self.archive}", "r:gz") as tar:
                 tar.extractall(path=self.script_path)
         elif "zip" in self.archive:
-            with zipfile.ZipFile(f"{self.archive}", 'r') as zip:
-                zip.extractall(path=self.script_path)
+            with zipfile.ZipFile(f"{self.archive}", 'r') as cmake_zip:
+                cmake_zip.extractall(path=self.script_path)
         else:
             print("Unsupported archive: {self.archive}", flush=True)
-            exit(1)
+            sys.exit(1)
         subprocess.run(os.path.join(self.path, "cmake --version"), shell=True, check=True)
 
 
     def set_path(self):
         if "GITHUB_PATH" in os.environ:
             print(f"Write CMake path, {self.path}, to {os.environ['GITHUB_PATH']}", flush=True)
-            with open(os.environ["GITHUB_PATH"], "a") as envfile:
+            with open(os.environ["GITHUB_PATH"], "a", encoding="utf-8") as envfile:
                 envfile.write(self.path)
 
 
@@ -169,7 +180,8 @@ def main():
     cmake_args = parser.parse_args()
 
     if cmake_args.test:
-        subprocess.run(os.path.join(os.path.dirname(__file__), "install_cmake_tests.py"), shell=True, check=True)
+        subprocess.run(os.path.join(os.path.dirname(__file__),
+            "install_cmake_tests.py"), shell=True, check=True)
 
     cmake_install = CMakeInstall(cmake_args.version, cmake_args.rc)
     if cmake_install.requested_cmake_is_different():
--- install_cmake_tests.py
+++ install_cmake_tests.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import unittest
-from install_cmake import *
+from install_cmake import CMakeInstall
 
 class TestMethods(unittest.TestCase):
     def test_get_installed_cmake_version(self):
@@ -10,11 +14,17 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(cmake_install.get_installed_cmake_version("blah"), "")
         self.assertEqual(cmake_install.get_installed_cmake_version("3"), "")
         self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 3.18"), "")
-        self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 3.18.4"), "3.18.4")
-        self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 3.19.0-rc3"), "3.19.0-rc3")
-        self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 99.99.99"), "99.99.99")
-        self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 99.99.99-rc99"), "99.99.99-rc99")
-        self.assertEqual(cmake_install.get_installed_cmake_version("cmake version 3.19.0-rc3\n\nCMake suite maintained and supported by Kitware (kitware.com/cmake).\n"), "3.19.0-rc3")
+        self.assertEqual(cmake_install.get_installed_cmake_version(
+            "cmake version 3.18.4"), "3.18.4")
+        self.assertEqual(cmake_install.get_installed_cmake_version(
+            "cmake version 3.19.0-rc3"), "3.19.0-rc3")
+        self.assertEqual(cmake_install.get_installed_cmake_version(
+            "cmake version 99.99.99"), "99.99.99")
+        self.assertEqual(cmake_install.get_installed_cmake_version(
+            "cmake version 99.99.99-rc99"), "99.99.99-rc99")
+        self.assertEqual(cmake_install.get_installed_cmake_version(
+            "cmake version 3.19.0-rc3\n\n"
+            "CMake suite maintained and supported by Kitware (kitware.com/cmake).\n"), "3.19.0-rc3")
 
 
 if __name__ == '__main__':
